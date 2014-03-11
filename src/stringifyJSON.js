caf581f69fdf42286100903b7c2623a7d10dad15
zzmp <zmp@umich.edu>
2014-03-11 03:07:18 -0500

    stringifyJSON resolved corner cases stringifyJSON(undefined) stringifyJSON(func)

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,29 +4,33 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   // your code goes here
-  var key = arguments[1];
-  var listKey = function(key) {
-    return (key ? stringifyJSON(key) + ':' : '');
-  }
+  if (typeof obj === 'undefined' || typeof obj === 'function') return;
 
-  if (obj && typeof obj === 'object') {
-    var listElements = function(list, element, key) {
-      return (list ? list + ',' : '') + (typeof key === 'string' ? stringifyJSON(element, key) : stringifyJSON(element));
+  var recursor = function(obj, key) {
+    var listKey = function(key) {
+      return (key ? recursor(key) + ':' : '');
     }
 
-    var bookend = Array.isArray(obj) ? ['[',']'] : ['{','}'];
-    return listKey(key) + bookend[0] + _.reduce(obj, listElements, '') + bookend[1];
-  } else {
-    // Data
-    var formatType = function(obj) {
-      if (typeof obj === 'string') return '"' + obj + '"';
-      else if (typeof obj === 'number') return obj;
-      else if (typeof obj === 'boolean') return obj;
-      else if (typeof obj === 'object') return 'null'; // null case
-      else if (typeof obj === 'undefined') return 'null';
-      // object/array are covered in previous block
-    }
+    if (obj && typeof obj === 'object') {
+      var listElements = function(list, element, key) {
+        return (list ? list + ',' : '') + (typeof key === 'string' ? recursor(element, key) : recursor(element));
+      }
 
-    return listKey(key) + formatType(obj);
-  }
+      var bookend = Array.isArray(obj) ? ['[',']'] : ['{','}'];
+      return listKey(key) + bookend[0] + _.reduce(obj, listElements, '') + bookend[1];
+    } else {
+      // Data
+      var formatType = function(obj) {
+        if (typeof obj === 'string') return '"' + obj + '"';
+        else if (typeof obj === 'number') return obj;
+        else if (typeof obj === 'boolean') return obj;
+        else if (typeof obj === 'object') return 'null'; // null case
+        else if (typeof obj === 'undefined') return 'null';
+        // object/array are covered in previous block
+      }
+
+      return listKey(key) + formatType(obj);
+    }
+  };
+  return recursor(obj);
 };
