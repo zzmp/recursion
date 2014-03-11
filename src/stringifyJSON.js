aa5ecc513202ff9c3f87b018e25d3bc832e2d4af
zzmp <zmp@umich.edu>
2014-03-11 02:42:01 -0500

    stringifyJSON resolved nesting issue

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,16 +4,20 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   // your code goes here
+  var key = arguments[1];
+  var listKey = function(key) {
+    return (key ? stringifyJSON(key) + ':' : '');
+  }
+
   if (obj && typeof obj === 'object') {
     var listElements = function(list, element, key) {
       return (list ? list + ',' : '') + (typeof key === 'string' ? stringifyJSON(element, key) : stringifyJSON(element));
     }
 
     var bookend = Array.isArray(obj) ? ['[',']'] : ['{','}'];
-    return bookend[0] + _.reduce(obj, listElements, '') + bookend[1];
+    return listKey(key) + bookend[0] + _.reduce(obj, listElements, '') + bookend[1];
   } else {
     // Data
-    var key = arguments[1];
-    return (key ? stringifyJSON(key) + ':' : '') + (typeof obj === 'string' ? '"' + obj + '"' : obj);
+    return listKey(key) + (typeof obj === 'string' ? '"' + obj + '"' : obj);
   }
 };
